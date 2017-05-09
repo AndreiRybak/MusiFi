f04556a4b3c53c1e9ddf8d176084f9147c431538
Andrei Rybak <Andrei_Rybak@epam.com>
2017-05-09 16:15:12 +0300

    Cleaned up code. Changed animation method for repeated animation.

--- MusiFi/HomePlayer/ShareViewController.swift
+++ MusiFi/HomePlayer/ShareViewController.swift
@@ -18,17 +18,23 @@ class ShareViewController: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet fileprivate weak var progressiveView: KDCircularProgress!
     @IBOutlet fileprivate weak var shareButton: UIButton!
+    @IBOutlet fileprivate weak var shareButtonImage: UIImageView!
     
     @IBOutlet fileprivate weak var noteView: UIView!
     @IBOutlet fileprivate weak var noteHeader: UILabel!
+    @IBOutlet fileprivate weak var noteImage: UIImageView!
     
     fileprivate let locationManager = CLLocationManager()
     fileprivate var latitude: Double = 0.0
     fileprivate var longitude: Double = 0.0
     
-    var nowPlayingInfo:[String:Any] = [:]
+    internal var nowPlayingInfo:[String:Any] = [:]
     
-    @IBOutlet weak var shareButtonImage: UIImageView!
+    struct Colors {
+        static let dark = UIColor(red: 21/255, green: 21/255, blue: 21/255, alpha: 1)
+        static let gray = UIColor(red: 39/255, green: 39/255, blue: 39/255, alpha: 1)
+        static let orange = UIColor(red: 246/255, green: 166/255, blue: 12/255, alpha: 1)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,35 +48,36 @@ class ShareViewController: UIViewController, CLLocationManagerDelegate {
     
     fileprivate func configureUI() {
         
-        self.view.backgroundColor = UIColor(red: 21/255, green: 21/255, blue: 21/255, alpha: 1)
+        self.view.backgroundColor = Colors.dark
         
-        musiFiLabel.textColor = UIColor(red: 251/255, green: 155/255, blue: 51/255, alpha: 1)
+        musiFiLabel.textColor = Colors.orange
         
         shareButton.layer.cornerRadius = 0.5 * shareButton.bounds.size.width
         
         progressiveView.angle = 0
-        progressiveView.trackColor = UIColor(red: 246/255, green: 166/255, blue: 12/255, alpha: 1)
-        progressiveView.progressInsideFillColor = UIColor(red: 246/255, green: 166/255, blue: 12/255, alpha: 1)
+        progressiveView.trackColor = Colors.orange
+        progressiveView.progressInsideFillColor = Colors.orange
         
-        noteView.backgroundColor = UIColor(red: 39/255, green: 39/255, blue: 39/255, alpha: 1)
+        noteView.backgroundColor = Colors.gray
         noteView.layer.cornerRadius = 16
         setAttributedText()
         
-        tabBarController?.tabBar.tintColor = UIColor(red: 251/255, green: 155/255, blue: 51/255, alpha: 1)
-        tabBarController?.tabBar.barTintColor = UIColor(red: 21/255, green: 21/255, blue: 21/255, alpha: 1)
-    
+        tabBarController?.tabBar.tintColor = Colors.orange
+        tabBarController?.tabBar.barTintColor = Colors.dark
+        
+        startRotateAnimation(image: noteImage, repeating: true)
     }
     
-    fileprivate func  setAttributedText() {
+    fileprivate func setAttributedText() {
         
         let musiFiString = NSString(string:"Share your music with MusiFi")
         let attributedString = NSMutableAttributedString(string: musiFiString as String)
         let range = musiFiString.range(of: "MusiFi")
-        attributedString.addAttribute(NSForegroundColorAttributeName, value: UIColor(red: 246/255, green: 166/255, blue: 12/255, alpha: 1), range: range)
+        attributedString.addAttribute(NSForegroundColorAttributeName, value: Colors.orange, range: range)
         noteHeader.attributedText = attributedString
     }
     
-    fileprivate func startAnimateShareButton() {
+    fileprivate func startRotateAnimation(image: UIImageView, repeating: Bool) {
         let fullRotation = CGFloat(Double.pi * 2)
         let duration = 1.7
         let delay = 0.0
@@ -79,17 +86,19 @@ class ShareViewController: UIViewController, CLLocationManagerDelegate {
         UIView.animateKeyframes(withDuration: duration, delay: delay, options: options, animations: {
             
             UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1/3, animations: {
-                self.shareButtonImage.transform = CGAffineTransform(rotationAngle: 1/3 * fullRotation)
+                image.transform = CGAffineTransform(rotationAngle: 1/3 * fullRotation)
             })
             UIView.addKeyframe(withRelativeStartTime: 1/3, relativeDuration: 1/3, animations: {
-                self.shareButtonImage.transform = CGAffineTransform(rotationAngle: 2/3 * fullRotation)
+                image.transform = CGAffineTransform(rotationAngle: 2/3 * fullRotation)
             })
             UIView.addKeyframe(withRelativeStartTime: 2/3, relativeDuration: 1/3, animations: {
-                self.shareButtonImage.transform = CGAffineTransform(rotationAngle: 3/3 * fullRotation)
+                image.transform = CGAffineTransform(rotationAngle: 3/3 * fullRotation)
             })
             
-        }, completion: {finished in
-            
+        }, completion: { finished in
+            if repeating {
+                self.startRotateAnimation(image: image, repeating: repeating)
+            }
         })
     }
     
@@ -100,7 +109,7 @@ class ShareViewController: UIViewController, CLLocationManagerDelegate {
     @IBAction func shareButtonPressed(_ sender: UIButton?) {
         
         shareButton.isEnabled = false
-        startAnimateShareButton()
+        startRotateAnimation(image: shareButtonImage, repeating: false)
         
         progressiveView.animate(toAngle: 360, duration: 1.5) { [weak self] (complete) in
             guard let strongSelf = self else { return }
