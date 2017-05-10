49d35802a1e5ce77da71b2b42aee3316928ea2ea
Andrei Rybak <Andrei_Rybak@epam.com>
2017-05-10 19:27:44 +0300

    Implemented custom point annotation.

--- MusiFi/MusicMap/MusicMapViewController.swift
+++ MusiFi/MusicMap/MusicMapViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MusicMapViewController: UIViewController, CLLocationManagerDelegate {
+class MusicMapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
+    
+    
+    //TODO: FAKE DATA
+    fileprivate let longitude = 21.2312312
+    fileprivate let latitude = 53.703173975086926
 
 
     override func viewDidLoad() {
@@ -22,8 +27,13 @@ class MusicMapViewController: UIViewController, CLLocationManagerDelegate {
         
         self.view.backgroundColor = Colors.dark
         
+        self.mapView.delegate = self
+        self.mapView.mapType = .hybrid
+        
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
+        
+        setAnnotationOnMap()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,12 +45,50 @@ class MusicMapViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         let location = locations[0]
-        let span = MKCoordinateSpanMake(0.2, 0.2)
+        let span = MKCoordinateSpanMake(0.05, 0.05)
         let myLocation = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
         let region = MKCoordinateRegionMake(myLocation, span)
         mapView.setRegion(region, animated: true)
         mapView.showsUserLocation = true
     }
-
+    
+    //TODO: SET ALL ANNOTATIONS FROM REQUSTED DATA
+    fileprivate func setAnnotationOnMap() {
+        let annotation =  MKPointAnnotation()
+        
+        annotation.coordinate.latitude = self.latitude
+        annotation.coordinate.longitude = self.longitude
+        annotation.title = "Annotation title"
+        annotation.subtitle = "Annotation subtitle"
+        self.mapView.addAnnotation(annotation)
+    }
+    
+    //MARK: MKMapViewDelegate methods
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if !(annotation is MKPointAnnotation) {
+            return nil
+        }
+        
+        let annotationIdentifier = "AnnotationIdentifier"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView!.canShowCallout = true
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        
+        //TODO: CHANGE PIN ICON
+        let pinImage = UIImage(named: "pin_annotation")
+        annotationView!.image = pinImage
+        
+        return annotationView
+    }
+    
+    
 }
