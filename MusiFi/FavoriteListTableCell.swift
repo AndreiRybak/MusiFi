f2b72ed3d6bc838e20e415ba31368e69c9469c9b
Andrei Rybak <Andrei_Rybak@epam.com>
2017-05-11 21:07:35 +0300

    Added favorite view controller.

---
+++ MusiFi/FavoriteListTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteListTableCell.swift
+//  MusiFi
+//
+//  Created by Andrei Rybak on 5/11/17.
+//  Copyright © 2017 Andrei Rybak. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteListTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
